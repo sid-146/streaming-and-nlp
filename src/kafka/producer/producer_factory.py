cfe133b4983f019becec12321df2990ed88dba1f
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-12-19 02:56:13 +0530

    Changed directory

--- src/kafka/producer/producer_factory.py
+++ src/kafka/producer/producer_factory.py
@@ -11,12 +11,12 @@ class KafkaProducerFactory:
             print(f"Message delivered to {msg.topic()} : {msg.key()}")
 
     @staticmethod
-    def create_producer(config: dict) -> Producer:
+    def create_producer(**config: dict) -> Producer:
         return Producer(
-            config,
-            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            **config,
+            # value_serializer=lambda v: json.dumps(v).encode("utf-8"),
         )
 
 
 if __name__ == "__main__":
-    print(KafkaProducerFactory.create_producer({"bootstrap.servers": "localhost:9092"}))
+    pass
