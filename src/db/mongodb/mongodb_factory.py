503d02c43b295155e7f8ed31a8594e2b399bdf3a
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-12-20 00:17:44 +0530

    - Added new function to create db client - renamed function

--- src/db/mongodb/mongodb_factory.py
+++ src/db/mongodb/mongodb_factory.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
+from pymongo.collection import Collection
 
 
 class MongoDBFactory:
     @staticmethod
-    def create_mongodb_client(config: dict) -> MongoClient:
+    def create_mongo_collection_client(config: dict) -> Collection:
         client = MongoClient(
             config["host"],
             config["port"],
@@ -12,6 +13,16 @@ class MongoDBFactory:
         )
         return client[config["database"]][config["collection"]]
 
+    @staticmethod
+    def create_mongodb_client(config) -> MongoClient:
+        client = MongoClient(
+            config["host"],
+            config["port"],
+            username=config["username"],
+            password=config["password"],
+        )
+        return client[config["database"]]
+
 
 if __name__ == "__main__":
-    mongo_client = MongoDBFactory.create_mongodb_client()
+    mongo_client = MongoDBFactory.create_mongo_collection_client()
