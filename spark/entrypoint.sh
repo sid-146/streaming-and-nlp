67614c674614387f86180e2d82d088924ce83c46
sid-146 <sudhanwa.kaveeshwar@gmail.com>
2025-12-24 17:08:41 +0530

    updated entrypoint and compose

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -3,21 +3,22 @@ set -e
 
 if [ "$SPARK_MODE" = "master" ]; then
     echo "Starting Spark Master"
-    
+
     exec /opt/spark/bin/spark-class \
-    org.apache.spark.deploy.master.Master \
-    --host 0.0.0.0 \
-    --port $MASTER_PORT \
-    --webui-port $WEBUI_PORT
-    
-    elif [ "$SPARK_MODE" = "worker" ]; then
+      org.apache.spark.deploy.master.Master \
+      --host 0.0.0.0 \
+      --port "$MASTER_PORT" \
+      --webui-port "$WEBUI_PORT"
+
+elif [ "$SPARK_MODE" = "worker" ]; then
     echo "Starting Spark Worker"
-    
+
     exec /opt/spark/bin/spark-class \
-    org.apache.spark.deploy.worker.Worker \
-    spark://spark-stream-master:$MASTER_PORT \
-    --host 0.0.0.0 \
-    --port $WORKER_PORT
+      org.apache.spark.deploy.worker.Worker \
+      "$SPARK_MASTER_URL" \
+      --host 0.0.0.0 \
+      --port "$WORKER_PORT" \
+      --webui-port "$SPARK_WORKER_WEBUI_PORT"
 else
     echo "Invalid SPARK_MODE"
     exit 1
